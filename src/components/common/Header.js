0a008fc2dc52f28ac6d27da2d515039d660ede1d
Nestor Zepeda <NestorDZepeda@gmail.com>
2017-09-06 00:30:43 -0700

    Set up redux store

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -9,7 +9,6 @@ const Header = () => {
             <Link to="/about" activeClassName="active">About</Link>
             {"   |   "}
             <Link to="/courses" activeClassName="active">Courses</Link>
-
         </nav>
     );
 };
